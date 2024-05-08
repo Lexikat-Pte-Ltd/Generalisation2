0404311a90e8d2da0ad2f56ffa42b97dceb42ae9
let-me-cook <itsalfath123@gmail.com>
2024-05-08 15:41:17 +0800

    better modularization

---
+++ scripts/loop.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+
+import docker
+from dotenv import load_dotenv
+from openai import OpenAI
+
+from lib.container import safe_detect_env, safe_filelist, safe_folderlist_bfs
+from lib.prompt import (
+    gen_skill_code_openai,
+    gen_skill_sets_openai,
+    prepare_codegen_prompt_openai,
+    prepare_taskgen_prompt_openai,
+)
+from lib.agent import run_loop
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    container_id = "5609a815862a"
+
+    docker_client = docker.from_env()
+    openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    out_con_path = Path("./data/controller/")
+    in_con_path = Path("/")
+    in_con_files = safe_folderlist_bfs(docker_client, container_id, in_con_path)
+    smol_in_con_files = in_con_files[:25] + ["..."] + in_con_files[-1:]
+
+    env_info = safe_detect_env(docker_client, container_id)
+
+    prompts = prepare_taskgen_prompt_openai(
+        env_info=env_info,
+        in_con_path=in_con_path,
+        in_con_files=in_con_files,
+    )
+
+    task_list = gen_skill_sets_openai(openai_client, prompts)
+
+    # for task in task_list:
+    task = task_list[0]
+
+    initial_prompts = prepare_codegen_prompt_openai(
+        task, env_info=env_info, in_con_path=in_con_path, in_con_files=smol_in_con_files
+    )
+    initial_code = gen_skill_code_openai(openai_client, initial_prompts)
+
+    run_loop(
+        docker_client=docker_client,
+        openai_client=openai_client,
+        container_id=container_id,
+        task_description=task,
+        initial_code=initial_code,
+        initial_prompts=initial_prompts,
+        skill_folder=out_con_path,
+    )
